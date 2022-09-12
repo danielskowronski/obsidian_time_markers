231ca442e9bf182b16c107e15e5562cd44629850
Daniel Skowroński <daniel@skowron.ski>
2022-09-12 03:22:27 +0200

    improve time parsing

--- view.ts
+++ view.ts
@@ -29,9 +29,10 @@ export class TimeMarkersView extends ItemView {
 	}
 
 	parseNote(){
-		window.APP = this.app;
+		window.APP = this.app; /* ugly debug */
+		this.initView();
+
 		try {
-			this.initView();
 			const container = this.containerEl.children[1];
 			const leafID = this.app.workspace.activeLeaf.id;
 
@@ -41,25 +42,36 @@ export class TimeMarkersView extends ItemView {
 
 			for (let lineNum=0; lineNum<editorObj.lineCount(); lineNum++){
 				let lineText = editorObj.getLine(lineNum);
-				let lineTimeMarkerMatch = lineText.match(/.*[0-9]{1,2}:[0-9]{2}.*/g);
+				let lineTimeMarkerMatch = lineText.match(/^[0-2]?[0-9]:[0-5][0-9]\s+.*|.*\s+[0-2]?[0-9]:[0-5][0-9]\s+.*|.*\s+[0-2]?[0-9]:[0-5][0-9]$/g); 
+				// `/TIME_ON_START_OF_LINE|TIME_IN_MIDDLE_OF_LINE|TIME_AT_END_OF_LINE/g``
+				// `TIME_...` is 24hrs time in HH:MM or H:MM form surrouned by whitespace or line start/end
 
 				if (lineTimeMarkerMatch) {
-					let formattedText = lineTimeMarkerMatch[0].
-						replace(/([^0-9])([0-9]{1}):([0-9]{2})/g, "$10$2:$3").
-						replace(/^- /, "").replace(/^\[.\] /, "");
-					let timeRaw = lineTimeMarkerMatch[0].match(/[0-9]{1,2}:[0-9]{2}/)[0];
-					let time = formattedText.match(/[0-9]{2}:[0-9]{2}/)[0];
+					let lineTimeMarker = lineTimeMarkerMatch[0]; // only first marker in line - this is opinionated assumption
+					let isCompletedTask = lineTimeMarker.match(/^- \[[\-xX]\]/);
+					if (! isCompletedTask) {
+						let formattedText = lineTimeMarker.
+							replace(/([^0-9])([0-9]{1}):([0-9]{2})/g, "$10$2:$3"). // convert H:MM to HH:MM
+							replace(/^- /, "").replace(/^\[.\] /, ""); // remove task list prefixes (`- [ ] `)
+						
+						let timeRaw = lineTimeMarker.match(/[0-9]{1,2}:[0-9]{2}/)[0];
+						let time = formattedText.match(/[0-9]{2}:[0-9]{2}/)[0];
 
-					let pos = lineTimeMarkerMatch[0].indexOf(timeRaw)+timeRaw.length+1;
+						let pos = lineTimeMarker.indexOf(timeRaw)+timeRaw.length;
+						if (pos<lineTimeMarker.length) { 
+							pos++; // only set cursor one character after time if time was not the last thing in line
+						}
 
-					let timeMarker = {
-						leafID: leafID,
-						line: lineNum,
-						ch: pos,
-						time: time,
-						formattedText: formattedText,
+						let timeMarker = {
+							leafID: leafID,
+							line: lineNum,
+							ch: pos,
+							time: time,
+							formattedText: formattedText,
+						}
+
+						timeMarkers[time] = timeMarker;
 					}
-					timeMarkers[time] = timeMarker;
 				}
 			}
 
@@ -67,8 +79,10 @@ export class TimeMarkersView extends ItemView {
 				let link = container.createEl("li", { }).
 					createEl("a", { 
 						text: timeMarker.formattedText, 
+						href: "#"
 					}
 				);
+
 				link.onClickEvent(() => {
 					this.jumpToLine(leafID, timeMarker.line, timeMarker.ch)
 				});
@@ -81,8 +95,6 @@ export class TimeMarkersView extends ItemView {
 	}
 
 	async onOpen() {
-		this.initView();
-
 		this.parseNote();
 	}
 
